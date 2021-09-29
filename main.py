6b2f69e247e1bca875671f62e62d94c700f5315d
Calm-Rock <chitgupta24@gmail.com>
2021-09-30 02:48:28 +0530

    Added live updated database

--- main.py
+++ main.py
@@ -18,14 +18,15 @@ response = urlopen(url)
 #Live data in json format
 data_json = json.loads(response.read())
 
-
-lvd1 = data_json['data']['boosts']['userData'] #lvd -> live data
+ #live data Native Balance
+lvd1 = data_json['data']['boosts']['userData']
 lvd2 = pd.DataFrame.from_dict(lvd1).T
 lvd_nativeBal_sum = sum(lvd2['nativeBalance'])
 
-lvd3 = data_json['data']['boosts']['userData'] #lvd -> live data
+#live data Non Native Balance
+lvd3 = data_json['data']['boosts']['userData'] 
 lvd4 = pd.DataFrame.from_dict(lvd3).T
-print(lvd4)
+#print(lvd4)
 lvd_non_nativeBal_sum = sum(lvd4['nonNativeBalance'])
 
 #Title
@@ -36,13 +37,23 @@ st.title('Badger Boost Data Visualization')
 def createList(r1, r2):
     return [item for item in range(r1, r2+1)]
 
-#CHANGE INDEXING 
+
 df = pd.read_csv('Native_Balance_Historical.csv')
-df_temp = df['Native Balance'].append(pd.Series(lvd_nativeBal_sum), ignore_index =True)
+if list(df['Native Balance'])[-1] == lvd_nativeBal_sum:
+	df_temp = df
+else:
+	df_temp = df['Native Balance'].append(pd.Series(lvd_nativeBal_sum), ignore_index =True)
+	
+#Appended Data Frame Native Balance
 lvd_native_balance = pd.DataFrame(df_temp, columns = ['Native Balance'])
 
+#Appended Data Frame Non Native Balance
 df1 = pd.read_csv('Non_Native_Balance_Historical.csv')
-df1_temp = df1['Non Native Balance'].append(pd.Series(lvd_non_nativeBal_sum), ignore_index =True)
+if list(df1['Non Native Balance'])[-1] == lvd_non_nativeBal_sum:
+	df1_temp = df1
+else:
+	df1_temp = df1['Non Native Balance'].append(pd.Series(lvd_non_nativeBal_sum), ignore_index =True)
+
 lvd_non_Native_balance = pd.DataFrame(df1_temp, columns = ['Non Native Balance'])
 
 
@@ -51,7 +62,9 @@ st.line_chart(lvd_native_balance['Native Balance'])
 
 st.line_chart(lvd_non_Native_balance['Non Native Balance'])
 
-
+#Updating the csv files
+lvd_native_balance.to_csv('Native_Balance_Historical.csv')
+lvd_non_Native_balance.to_csv('Non_Native_Balance_Historical.csv')
 
 # Streamlit widgets automatically run the script from top to bottom. Since
 # this button is not connected to any other logic, it just causes a plain
